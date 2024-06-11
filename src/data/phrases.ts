9076aacbba6a7ac137106c125731d97c16ca4542
Kostia Trepyk <trepyk.kostiantynn@gmail.com>
2024-06-11 15:13:51 +0200

    Added phrases

---
+++ src/data/phrases.ts
@@ -0,0 +1,32 @@
+export interface Phrase {
+	phrase: string;
+	author: string;
+}
+
+export const phrases: Phrase[] = [
+	{
+		phrase: "The only way to do great work is to love what you do.",
+		author: "Steve Jobs",
+	},
+	{
+		phrase: "Well done is better than well said.",
+		author: "Benjamin Franklin",
+	},
+	{
+		phrase:
+			"If you're not prepared to be wrong, you'll never come up with anything original.",
+		author: "Ken Robinson",
+	},
+	{
+		phrase: "First, solve the problem. Then, write the code.",
+		author: "John Johnson",
+	},
+	{
+		phrase: "In the realm of code, curiosity is the key to every door.",
+		author: "Me",
+	},
+	{
+		phrase: "The best coders are those who never stop learning.",
+		author: "Me",
+	},
+];
