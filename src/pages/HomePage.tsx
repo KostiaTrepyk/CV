f4fb06980e75f14351cf7179f584cbea0ea0a7fe
Kostia Trepyk <trepyk.kostiantynn@gmail.com>
2024-06-11 15:21:04 +0200

    Added MeSection

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,16 +1,19 @@
 import { Box } from "@mui/material";
+import MeSection from "components/sections/MeSection";
 
 import IntroSection from "components/sections/IntroSection";
 import ProjectsSection from "components/sections/ProjectsSection";
 
 const HomePage = () => {
-    return (
-        <Box component="main">
-            <IntroSection />
+	return (
+		<Box component="main">
+			<IntroSection />
 
-            <ProjectsSection />
-        </Box>
-    );
+			<ProjectsSection />
+
+			<MeSection />
+		</Box>
+	);
 };
 
 export default HomePage;
