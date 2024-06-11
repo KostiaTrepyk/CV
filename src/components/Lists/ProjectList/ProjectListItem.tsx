5285b3ab54d0e49da822784958e22ff28c08da1d
Kostia Trepyk <trepyk.kostiantynn@gmail.com>
2024-06-11 15:14:54 +0200

    Updated PorjectSection

--- src/components/Lists/ProjectList/ProjectListItem.tsx
+++ src/components/Lists/ProjectList/ProjectListItem.tsx
@@ -6,145 +6,145 @@ import { type Variants, motion } from "framer-motion";
 const MBox = motion(Box);
 
 const animations: Variants = {
-    initial: {
-        y: 20,
-        scale: 0.95,
-    },
-    inView: {
-        y: 0,
-        scale: 1,
-    },
-    hover: {
-        y: -5,
-        scale: 0.98,
-    },
+	initial: {
+		y: 20,
+		scale: 0.95,
+	},
+	inView: {
+		y: 0,
+		scale: 1,
+	},
+	hover: {
+		y: -5,
+		scale: 0.98,
+	},
 };
 
 export interface Project {
-    title: string;
-    description: string;
-    technologies: string[];
-    link: string;
+	title: string;
+	description: string;
+	techs: string[];
+	link: string;
+	marked?: boolean;
 }
 
 interface ProjectListItemProps {
-    project: Project;
+	project: Project;
 }
 
 const ProjectListItem = forwardRef<HTMLLIElement, ProjectListItemProps>(
-    ({ project }, ref) => {
-        return (
-            <MBox
-                initial="initial"
-                whileInView="inView"
-                whileHover={"hover"}
-                variants={animations}
-                transition={{
-                    ease: "easeOut",
-                    duration: 0.25,
-                }}
-                sx={{
-                    flex: "1 1 320px",
-                    display: "flex",
-                    flexDirection: "column",
-                    gap: 3,
-                    paddingInline: 3,
-                    paddingBlock: 5,
-                    borderRadius: "2rem",
-                    maxWidth: "500px",
+	({ project }, ref) => {
+		return (
+			<MBox
+				initial="initial"
+				whileInView="inView"
+				whileHover={"hover"}
+				variants={animations}
+				transition={{
+					ease: "easeOut",
+					duration: 0.25,
+				}}
+				sx={{
+					flex: "1 1 320px",
+					display: "flex",
+					flexDirection: "column",
+					gap: 3,
+					paddingInline: 3,
+					paddingBlock: 5,
+					borderRadius: "2rem",
+					maxWidth: "500px",
 
-                    backgroundImage: (theme) =>
-                        `linear-gradient(-235deg, ${theme.palette.primary.dark} -100%, #ffffff10 10% 70%, ${theme.palette.primary.main} 300%)`,
-                    boxShadow: "inset 0px 0px 2px #fff1",
-                }}
-                component="li"
-                ref={ref}
-            >
-                <Box>
-                    <Typography
-                        sx={{
-                            textAlign: "center",
-                            fontSize: "1.8rem",
-                            fontWeight: 600,
-                            marginBottom: 1,
-                        }}
-                        variant="h3"
-                    >
-                        {project.title}
-                    </Typography>
+					backgroundImage: (theme) =>
+						`linear-gradient(-235deg, ${theme.palette.primary.dark} -120%, #ffffff18 20% 70%, ${theme.palette.primary.dark} 250%)`,
+					boxShadow: "inset 0px 0px 4px #0004",
+				}}
+				component="li"
+				ref={ref}
+			>
+				<Box>
+					<Typography
+						sx={{
+							textAlign: "center",
+							fontSize: "1.8rem",
+							fontWeight: 600,
+							marginBottom: 1,
+						}}
+						variant="h3"
+					>
+						{project.title}
+					</Typography>
 
-                    <Typography
-                        sx={{
-                            letterSpacing: "0.03rem",
-                            color: "text.secondary",
-                        }}
-                    >
-                        {project.description}
-                    </Typography>
-                </Box>
+					<Typography
+						sx={{
+							letterSpacing: "0.03rem",
+							color: "text.secondary",
+						}}
+					>
+						{project.description}
+					</Typography>
+				</Box>
 
-                <Stack
-                    sx={{
-                        listStyle: "none",
-                        margin: 0,
-                        padding: 0,
-                        display: "grid",
-                        gridTemplateColumns:
-                            "repeat(auto-fit, minmax(110px,1fr))",
-                        gridAutoRows: "1fr",
-                        gap: "0.5rem",
-                    }}
-                    component="ul"
-                >
-                    {project.technologies.map((technology) => (
-                        <Paper
-                            key={technology}
-                            sx={{
-                                textAlign: "center",
-                                padding: 1.5,
-                                borderRadius: "1rem",
-                                boxShadow: 0,
-                            }}
-                            elevation={10}
-                            component="li"
-                        >
-                            <Typography>{technology}</Typography>
-                        </Paper>
-                    ))}
-                </Stack>
+				<Stack
+					sx={{
+						listStyle: "none",
+						margin: 0,
+						padding: 0,
+						display: "grid",
+						gridTemplateColumns: "repeat(auto-fit, minmax(110px,1fr))",
+						gridAutoRows: "1fr",
+						gap: "0.5rem",
+					}}
+					component="ul"
+				>
+					{project.techs.map((tech) => (
+						<Paper
+							key={tech}
+							sx={{
+								textAlign: "center",
+								padding: 1.5,
+								borderRadius: "1rem",
+								boxShadow: 0,
+							}}
+							elevation={10}
+							component="li"
+						>
+							<Typography>{tech}</Typography>
+						</Paper>
+					))}
+				</Stack>
 
-                <Link
-                    style={{
-                        textDecoration: "none",
-                        marginTop: "auto",
-                        marginInline: "auto",
+				<Link
+					style={{
+						textDecoration: "none",
+						marginTop: "auto",
+						marginInline: "auto",
 
-                        cursor: "pointer",
-                    }}
-                    to={project.link}
-                    target="_blank"
-                >
-                    <Box
-                        sx={{
-                            display: "block",
-                            textTransform: "uppercase",
-                            paddingBlock: "1rem",
-                            paddingInline: "1.2rem",
-                            borderRadius: "1.5rem",
-                            fontWeight: 600,
-                            fontSize: "0.9rem",
+						cursor: "pointer",
+					}}
+					to={project.link}
+					target="_blank"
+				>
+					<Box
+						sx={{
+							display: "block",
+							textTransform: "uppercase",
+							paddingBlock: "1rem",
+							paddingInline: "1.2rem",
+							borderRadius: "1.5rem",
+							fontWeight: 600,
+							fontSize: "0.9rem",
 
-                            color: "primary.contrastText",
-                            backgroundColor: "primary.main",
-                        }}
-                        component="span"
-                    >
-                        Check out
-                    </Box>
-                </Link>
-            </MBox>
-        );
-    }
+							color: "primary.contrastText",
+							backgroundColor: "primary.main",
+						}}
+						component="span"
+					>
+						Check out
+					</Box>
+				</Link>
+			</MBox>
+		);
+	}
 );
 
 export default ProjectListItem;
--- src/components/sections/ProjectsSection.tsx
+++ src/components/sections/ProjectsSection.tsx
@@ -1,96 +1,72 @@
 import { forwardRef } from "react";
 import { Link } from "react-router-dom";
 import { Container, Typography } from "@mui/material";
-import { chatLink, cloudStorageClientLink, movieLibraryLink } from "data/links";
+import { motion } from "framer-motion";
+import { projects } from "data/projects";
 
 import ProjectList from "components/Lists/ProjectList/ProjectList";
-import ProjectListItem, {
-  type Project,
-} from "components/Lists/ProjectList/ProjectListItem";
+import ProjectListItem from "components/Lists/ProjectList/ProjectListItem";
+
+const MContainer = motion(Container);
 
 interface ProjectsSectionProps {}
 
 const ProjectsSection = forwardRef<HTMLElement, ProjectsSectionProps>(
-  (_, ref) => {
-    const projects: Project[] = [
-      {
-        title: "Movie Library",
-        description:
-          "Embark on a cinematic journey like never before with Movie Library – where every movie and serial is just a click away. Start exploring, discovering, and enjoying the magic of storytelling today!",
-        technologies: ["React", "Redux", "Typescript", "Material UI"],
-        link: movieLibraryLink,
-      },
-      {
-        title: "Cloud Storage",
-        description:
-          "Start managing your data with ease and efficiency today! Cloud Storage is a full-stack application that allows users to create disks, folders, and files in a cloud-based environment.",
-        technologies: [
-          "React",
-          "Redux",
-          "Storybook",
-          "Typescript",
-          "NestJs",
-          "JWT",
-        ],
-        link: cloudStorageClientLink,
-      },
-      {
-        title: "Chat",
-        description:
-          "A simple fullstack application that allows users to create new chats and communicate with others. This is my first project. Created mainly for developing skills in working with back-end and learning how to work with web sockets.",
-        technologies: ["React", "Typescript", "WebSocket", "JWT"],
-        link: chatLink,
-      },
-    ];
+	(_, ref) => {
+		const topProjects = projects.slice(0, 3);
 
-    return (
-      <Container
-        sx={{
-          py: { sm: 4, xs: 2 },
-        }}
-        component="section"
-        ref={ref}
-      >
-        <Typography
-          sx={{
-            textAlign: "center",
-            fontSize: "1.8rem",
-          }}
-          variant="h2"
-        >
-          Selected projects
-        </Typography>
+		return (
+			<MContainer
+				sx={{
+					py: { sm: 4, xs: 2 },
+				}}
+				// @ts-ignore
+				component="section"
+				initial={{ opacity: 0 }}
+				whileInView={{ opacity: 1 }}
+				transition={{ duration: 0.5 }}
+				ref={ref}
+			>
+				<Typography
+					sx={{
+						textAlign: "center",
+						fontSize: "1.8rem",
+					}}
+					variant="h2"
+				>
+					Selected projects
+				</Typography>
 
-        <Typography
-          sx={{
-            color: "text.disabled",
-            textAlign: "center",
-            marginBottom: "1.5rem",
-            fontSize: "1.2rem",
-          }}
-        >
-          Visit my{" "}
-          <Typography
-            sx={{
-              fontSize: "inherit",
-              color: "primary.main",
-              textDecoration: "underline",
-            }}
-            component="span"
-          >
-            <Link to={"https://github.com/KostiaTrepyk"}>Github</Link>
-          </Typography>{" "}
-          to see more
-        </Typography>
+				<Typography
+					sx={{
+						color: "text.disabled",
+						textAlign: "center",
+						marginBottom: "1.5rem",
+						fontSize: "1.2rem",
+					}}
+				>
+					Visit my{" "}
+					<Typography
+						sx={{
+							fontSize: "inherit",
+							color: "primary.main",
+							textDecoration: "underline",
+						}}
+						component="span"
+					>
+						<Link to={"https://github.com/KostiaTrepyk"}>Github</Link>
+					</Typography>{" "}
+					to see more
+				</Typography>
 
-        <ProjectList>
-          {projects.map((project) => (
-            <ProjectListItem key={project.title} project={project} />
-          ))}
-        </ProjectList>
-      </Container>
-    );
-  }
+				<ProjectList>
+					{topProjects.map((project) => (
+						<ProjectListItem key={project.title} project={project} />
+					))}
+				</ProjectList>
+			</MContainer>
+		);
+	}
 );
 
 export default ProjectsSection;
